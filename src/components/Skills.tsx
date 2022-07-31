2d2b93c0e2b42f4495e90114dbc859d59a825041
alvfaye <alvfaye@gmail.com>
2022-07-31 21:50:40 +0800

    update skills

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,7 +1,4 @@
-// src/components/Skills.js
-
 import { BadgeCheckIcon, ChipIcon } from '@heroicons/react/solid';
-import React from 'react';
 import { skills } from '../data';
 
 export default function Skills() {
